cc2908d6fcdf3e6d70fd85eef174db0c6bd8c892
Aditya Singh <aditya.singh.lko2002@gmail.com>
2024-04-03 05:51:35 +0530

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -31,9 +31,9 @@ const Sidebar: React.FC = () => {
       </div>
 
       
-      <ul className="space-y-2 pt-4 text-md px-2">
+      <ul className="space-y-2 pt-4  text-md px-2">
         {menuItems.map((item, index) => (
-          <li key={index}>
+          <li key={index} className='py-3'>
             <FontAwesomeIcon icon={item.icon} className="mr-2" /> {item.label}
           </li>
         ))}
