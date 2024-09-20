e44d37930acc9d6013c0fd8814c14a68b89a9e24
Aditya Singh <adityasinghtech011@gmail.com>
2024-09-20 23:42:54 +0530

    intial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'dist', // This sets the output directory to 'dist'
+};
 
 export default nextConfig;
