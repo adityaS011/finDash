a16ff42bf908fc5ed3fd4fb4a78921751d3333c5
Aditya Singh <adityasinghtech011@gmail.com>
2024-10-22 05:46:09 +0530

    version-0.0.2

--- app/(dashboard-pages)/layout.tsx
+++ app/(dashboard-pages)/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
 import Sidebar from '../components/Sidebar';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
-  title: 'Fin Dashboard',
-  description: 'Automation for your growing business',
+  title: 'Finance Repo',
+  description: 'Manage your finances',
 };
 
 export default async function Layout({
@@ -13,16 +14,20 @@ export default async function Layout({
 }) {
   return (
     <>
+      <Head>
+        <title>Finance Repo</title>
+        <meta name='description' content='Manage your finances' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+
       <div className='flex flex-col md:flex-row h-full bg-gray-200 text-gray-800'>
-        {/* Sidebar, fixed to the left */}
-        <div className='fixed w-fit z-50 border-l border border-gray-300'>
+        <div className='md:fixed md:w-52 z-50 border-r border-gray-300'>
           <Sidebar />
         </div>
-        <hr className='h-0.5 bg-slate-300' />
 
-        {/* Main content area */}
         <div className='flex-grow md:ml-52 mt-6 md:mt-0 h-screen overflow-auto no-scrollbar'>
-          {children} {/* This will render the content of each page */}
+          {children}
         </div>
       </div>
     </>
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,33 +1,53 @@
 'use client';
-import { usePathname, useRouter } from 'next/navigation'; // Import usePathname and useRouter
+import { usePathname, useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
-import {
-  faHome,
-  faHistory,
-  faMoneyBillWave,
-  faAddressBook,
-  faHandHoldingUsd,
-  faCog,
-  faExclamationCircle,
-} from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
+import {
+  CircleExclamationIcon,
+  ContactIcon,
+  FinanceIcon,
+  HomeIcon,
+  LogoIcon,
+  PaymentsIcon,
+  SettingIcon,
+  TimelineIcon,
+} from '../icons';
 
 interface MenuItem {
-  icon: IconDefinition;
+  icon: ReactNode;
   label: string;
   route: string;
 }
 
 const menuItems: MenuItem[] = [
-  { icon: faHome, label: 'Home', route: '/' },
-  { icon: faHistory, label: 'Timelines', route: '/timelines' },
-  { icon: faMoneyBillWave, label: 'Payments', route: '/payments' },
-  { icon: faAddressBook, label: 'Directory', route: '/directory' },
-  { icon: faHandHoldingUsd, label: 'Financials', route: '/financials' },
-  { icon: faCog, label: 'Settings', route: '/settings' },
+  { icon: <HomeIcon className='w-fit mr-1 h-5' />, label: 'Home', route: '/' },
+  {
+    icon: <TimelineIcon className='w-fit  h-6' />,
+    label: 'Timelines',
+    route: '/timelines',
+  },
+  {
+    icon: <PaymentsIcon className='w-fit  h-6' />,
+    label: 'Payments',
+    route: '/payments',
+  },
+  {
+    icon: <ContactIcon className='w-fit  h-6' />,
+    label: 'Directory',
+    route: '/directory',
+  },
+  {
+    icon: <FinanceIcon className='w-fit  h-6' />,
+    label: 'Financials',
+    route: '/financials',
+  },
+  {
+    icon: <SettingIcon className='w-fit mr-1 h-5' />,
+    label: 'Settings',
+    route: '/settings',
+  },
 ];
 
 const Sidebar = () => {
@@ -53,55 +73,47 @@ const Sidebar = () => {
   return (
     <>
       {/* Top Navbar for mobile */}
-      <div className='md:hidden z-100 fixed top-0 left-0 right-0 border-b border-l  shadow-md bg-gray-200 text-gray-700 flex justify-between items-center p-4 z-40'>
-        <Image src={'/logo.png'} alt='Fintech Logo' width={100} height={100} />
-        {/*<button
-          onClick={toggleSidebar}
-          className='text-gray-700 focus:outline-none'
-        >
-          {isOpen ? (
-            <FontAwesomeIcon icon={faRectangleXmark} size='lg' />
-          ) : (
-            <FontAwesomeIcon icon={faBars} size='lg' />
-          )}
-        </button>*/}
+      <div className='md:hidden z-100 fixed top-0 left-0 right-0 border-b border-l  shadow-md bg-[#ecfae5] text-gray-700 flex justify-between items-center p-4 z-40'>
+        <LogoIcon className='w-7 h-7 rounded-full' />{' '}
+        <p className=' text-gray-600 font-sans tracking-wide uppercase text-lg font-semibold '>
+          Finance Repo
+        </p>
       </div>
 
       {/* Sidebar for larger screens */}
       <div
         className={`${
           isOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0 transition-transform fixed md:relative top-0 left-0 h-screen overflow-hidden bg-gray-200 text-gray-700 w-52 p-4 z-40`}
+        } md:translate-x-0 transition-transform fixed md:relative top-1 left-0 h-screen overflow-hidden bg-gray-200 text-gray-700 w-52 px-4 py-2 z-40`}
       >
-        <div className='rounded flex w-32 h-12 p-2 mt-2 mb-4'>
-          <Image
-            src={'/logo.png'}
-            alt='Fintech Logo'
-            width={100}
-            height={100}
-          />
+        <div className='rounded-3xl border border-[#efeaea] shadow bg-[#ecfae5] flex w-40 h-12 p-2 mt-2 mb-4 gap-2 hover:translate-x-[-1px] hover:translate-y-[-1px] cursor-pointer'>
+          <LogoIcon className='w-7 h-7 rounded-full' />
+          <p className=' text-gray-600 font-sans tracking-tight text-lg font-semibold '>
+            Finance Repo
+          </p>
         </div>
 
         <ul className='space-y-2 pt-4 text-md px-2'>
           {menuItems.map((item, index) => (
             <li
               key={index}
-              className={`py-2 cursor-pointer ${getTextColor(item.route)}`} // Apply dynamic color
+              className={`py-1 rounded-lg hover:text-white hover:opacity-95 hover:bg-[#60c689] cursor-pointer ${getTextColor(
+                item.route
+              )}`}
               onClick={() => handleNavigation(item.route)}
             >
-              <span className='icon-wrapper'>
-                <FontAwesomeIcon icon={item.icon} className='mr-2' />{' '}
-                {item.label}
-              </span>
+              <div className='flex flex-row gap-2 p-2 items-center max-w-24 w-fit'>
+                <p className=' w-fit'>{item.icon}</p>
+                <p className='text-lg tracking-wide font-sans'>{item.label}</p>
+              </div>
             </li>
           ))}
         </ul>
 
         <hr className='h-0.5 bg-slate-300' />
 
-        <button className='bg-black text-white font-semibold p-2 m-4 rounded-l-full rounded-br-full absolute bottom-0 left-0'>
-          <FontAwesomeIcon icon={faExclamationCircle} className='mr-2' />
-          Get Help
+        <button className='flex flex-row  gap-2 items-center hover:translate-x-[-1px] hover:translate-y-[-1px] cursor-pointer bg-black text-white font-semibold p-2 m-4 rounded-l-full rounded-br-full absolute bottom-0 left-0'>
+          <CircleExclamationIcon className='w-5 h-5' /> <p>Get Help</p>
         </button>
       </div>
 
@@ -121,7 +133,7 @@ const Sidebar = () => {
             className={`text-gray-700 ${getTextColor(item.route)}`} // Apply dynamic color for mobile
             onClick={() => handleNavigation(item.route)}
           >
-            <FontAwesomeIcon icon={item.icon} size='lg' />
+            {item.icon}
           </button>
         ))}
       </div>
